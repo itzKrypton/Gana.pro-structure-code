11949838a81b3653d9c773e27cab46947bf5f785
Sahil Arun <151955025+sahilarun@users.noreply.github.com>
2024-07-20 15:17:26 +0530

    Update developers.js

--- src/commands/General/developers.js
+++ src/commands/General/developers.js
@@ -1,8 +1,8 @@
 const { EmbedBuilder, ButtonBuilder, ActionRowBuilder, ButtonStyle } = require("discord.js");
 module.exports = {
-  name: 'developer',
+  name: 'developers',
   category: 'Information',
-  aliases: ['dev'],
+  aliases: ['dev', 'developer'],
   cooldown: 5,
   description: '',
   args: false,
@@ -11,5 +11,32 @@ module.exports = {
   botPrams: ['EmbedLinks'],
   owner: false,
   execute: async (message, args, client, prefix) => {
+
+    const sahil1 = new ActionRowBuilder().addComponents(
+       new ButtonBuilder().setStyle(ButtonStyle.Danger).setCustomId(`sahil`).setLabel(`アバヤンカラ`).setDisabled(true),
+       new ButtonBuilder().setStyle(ButtonStyle.Secondary).setCustomId(`flexx`).setLabel(`Nitrix`),
+       new ButtonBuilder().setStyle(ButtonStyle.Secondary).setCustomId(`devil`).setLabel(`Devil`),
+       new ButtonBuilder().setStyle(ButtonStyle.Secondary).setCustomId(`roonee`).setLabel(`Roonee`)
+    )
+    const flexx1 = new ActionRowBuilder().addComponents(
+       new ButtonBuilder().setStyle(ButtonStyle.Secondary).setCustomId(`sahil`).setLabel(`アバヤンカラ`),
+       new ButtonBuilder().setStyle(ButtonStyle.Danger).setCustomId(`flexx`).setLabel(`Nitrix`).setDisabled(true),
+       new ButtonBuilder().setStyle(ButtonStyle.Secondary).setCustomId(`devil`).setLabel(`Devil`),
+       new ButtonBuilder().setStyle(ButtonStyle.Secondary).setCustomId(`roonee`).setLabel(`Roonee`)
+     )
+     const devil1 = new ActionRowBuilder().addComponents(
+        new ButtonBuilder().setStyle(ButtonStyle.Secondary).setCustomId(`sahil`).setLabel(`アバヤンカラ`),
+       new ButtonBuilder().setStyle(ButtonStyle.Secondary).setCustomId(`flexx`).setLabel(`Nitrix`),
+       new ButtonBuilder().setStyle(ButtonStyle.Danger).setCustomId(`devil`).setLabel(`Devil`).setDisabled(true),
+       new ButtonBuilder().setStyle(ButtonStyle.Secondary).setCustomId(`roonee`).setLabel(`Roonee`)
+      ) 
+    const roonee1 = new ActionRowBuilder().addComponents(
+      new ButtonBuilder().setStyle(ButtonStyle.Secondary ).setCustomId(`sahil`).setLabel(`アバヤンカラ`),
+       new ButtonBuilder().setStyle(ButtonStyle.Secondary).setCustomId(`flexx`).setLabel(`Nitrix`),
+       new ButtonBuilder().setStyle(ButtonStyle.Secondary).setCustomId(`devil`).setLabel(`Devil`),
+       new ButtonBuilder().setStyle(ButtonStyle.Danger).setCustomId(`roonee`).setLabel(`Roonee`).setDisabled(true)
+    )
   }
 }
+
+1
