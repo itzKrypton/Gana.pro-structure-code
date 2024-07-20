9711ca743cd1ff3fd8e6637c6eb673f8bfd428ce
Sahil Arun <151955025+sahilarun@users.noreply.github.com>
2024-07-20 15:30:32 +0530

    Update developers.js

--- src/commands/General/developers.js
+++ src/commands/General/developers.js
@@ -12,7 +12,7 @@ module.exports = {
   owner: false,
   execute: async (message, args, client, prefix) => {
 
-    let sahil = new AttachmentBuilder(`https://iili.io/dnYS3gV.jpg`);
+    let sahil2 = new AttachmentBuilder(`https://iili.io/dnYS3gV.jpg`);
     const sahil1 = new ActionRowBuilder().addComponents(
        new ButtonBuilder().setStyle(ButtonStyle.Danger).setCustomId(`sahil`).setLabel(`アバヤンカラ`).setDisabled(true),
        new ButtonBuilder().setStyle(ButtonStyle.Secondary).setCustomId(`flexx`).setLabel(`Nitrix`),
@@ -37,6 +37,40 @@ module.exports = {
        new ButtonBuilder().setStyle(ButtonStyle.Secondary).setCustomId(`devil`).setLabel(`Devil`),
        new ButtonBuilder().setStyle(ButtonStyle.Danger).setCustomId(`roonee`).setLabel(`Roonee`).setDisabled(true)
     )
-    const msg = await message.channel.send({fles: [sahil], components: [sahil1]});
+    const msg = await message.channel.send({fles: [sahil2], components: [sahil1]});
+    const collector = await msg.createMessageComponentCollector({
+                filter :(interaction) => {
+                    if(message.author.id === interaction.user.id) return true;
+                    else{
+                        interaction.reply({content : `Only ${message.author.tag} can use this button!` , ephemeral : true})
+                    }
+                },
+                time : 1000000,
+                idle : 1000000/2
+     });
+
+    if(interaction.isButton())
+                {
+                    if(interaction.customId === `sahil`)
+                    {
+                        return interaction.update({files : [sahil2], components: [sahil1]});
+                    }
+                    if(interaction.customId === `flexx`)
+                    {
+                        return interaction.update({files : [flexx2], components: [flexx1]});
+                    }
+                    if(interaction.customId === `devil`)
+                    {
+                        return interaction.update({files : [devil2], components: [devil1]})
+                    }
+                    if(interaction.customId === `roonee`)
+                    {
+                        return interaction.update({files : [roonee2], components: [roonie1]})
+                    }
+                }
+            });
+            collector.on('end',async() => {
+            msg.edit({files : [sahil2] , components : []})
+        })
   }
 }
